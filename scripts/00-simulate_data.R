87cb5817d3b90f9a08b17876c00c78a0cfbfe0a2
Sakura Ariga <sakura.ariga@mail.utoronto.ca>
2023-04-19 14:21:16 -0400

    Simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -11,3 +11,45 @@ library(tidyverse)
 
 
 #### Simulate data ####
+set.seed(853)
+
+artists_simulated_data <-
+  tibble(
+    year = c(1929:2023),
+    percentage_female = runif(
+      n = 95, 
+      min = 0, 
+      max = 100),
+    race = sample(
+      x = c(1:7),
+      size = 95,
+      replace = TRUE),
+    position = sample(
+      x = c("Artist"),
+      size = 95,
+      replace = TRUE)
+ )
+head(artists_simulated_data)
+
+directors_simulated_data <-
+  tibble(
+    year = c(1929:2023),
+    percentage_female = runif(
+      n = 95, 
+      min = 0, 
+      max = 100),
+    race = sample(
+      x = c(1:7),
+      size = 95,
+      replace = TRUE),
+    position = sample(
+      x = c("Director or Department Head"),
+      size = 95,
+      replace = TRUE)
+  )
+head(directors_simulated_data)
+
+
+#### Save simulated data ####
+write_csv(artists_simulated_data, "outputs/data/artists_simulated_data.csv")
+write_csv(directors_simulated_data, "outputs/data/directors_simulated_data.csv")
