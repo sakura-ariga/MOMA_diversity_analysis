03a5d02ac7be60a36e909459565e43535dc20679
Sakura Ariga <sakura.ariga@mail.utoronto.ca>
2023-04-19 14:29:30 -0400

    Created tests for simulated data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -10,4 +10,26 @@
 library(tidyverse)
 
 
+#### Download data ####
+artists_simulated_data <- read_csv("outputs/data/artists_simulated_data.csv")
+directors_simulated_data <- read_csv("outputs/data/directors_simulated_data.csv")
+
+
 #### Test data ####
+artists_simulated_data$percentage_female |> 
+  class() == "numeric"
+
+artists_simulated_data$year |> 
+  class() == "numeric"
+
+artists_simulated_data$year |> 
+  min() == 1929
+
+directors_simulated_data$year |> 
+  max() == 2023
+
+directors_simulated_data$percentage_female |> 
+  min() >= 0
+
+directors_simulated_data$percentage_female |> 
+  max() <= 100
